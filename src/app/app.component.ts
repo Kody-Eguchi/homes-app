dc8e85b04b64c8caee3e87d34e343a54a8ae559e
Koji Eguchi <koji.eguchi@hotmail.com>
2023-11-13 18:35:32 -0500

    Add a search bar to Home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import {Component} from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent],
   template: `
     <main>
       <header class="brand-name">
