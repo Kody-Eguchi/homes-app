135ddd9154860b2e26405ac6ce78112960592df1
Koji Eguchi <koji.eguchi@hotmail.com>
2023-11-13 18:30:52 -0500

    Generate Home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ import {Component} from '@angular/core';
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" arial-hidden="true">
       </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
     </main>
   `,
   styleUrls: ['./app.component.css'],
