0ca7e1374b213dab80b840db597ea58da2852eb7
Koji Eguchi <koji.eguchi@hotmail.com>
2023-11-14 18:40:29 -0500

    Add directives to iterate housing data in Housing component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
@@ -7,14 +8,14 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <section class="listing">
-      <img src="" alt="" class="listing-photo">
-      <h2 class="lisiting-heading"></h2>
-      <p class="listing-location"></p>
+      <img [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}" class="listing-photo">
+      <h2 class="lisiting-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
     </section>
   `,
   styles: [
   ]
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!:HousingLocation;
 }
